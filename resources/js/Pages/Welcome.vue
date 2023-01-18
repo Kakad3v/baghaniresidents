ee76b22140939a7132b5e70a063a0b522df7b0e9
Kakad3v <sule.appdev@gmail.com>
2023-01-18 23:00:31 +0300

    comment out

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -13,6 +13,9 @@ defineProps({
     <Head title="Welcome" />
 
     <div class="relative flex items-top justify-center min-h-screen bg-[#A3BB98] dark:bg-gray-900 sm:items-center sm:pt-0">
-<h1 class="text-4xl text-[#F0ECCF]">Coming Soon...</h1>
+        <div class="flex items-center space-x-5">
+        <h1 class="text-4xl text-[#F0ECCF]">Coming Soon...</h1>
+        <!-- <Link href="/register" class="bg-[#f0eccf] text-[#A3BB98] text-2xl rounded-xl p-2 border-4 border-[#03C988]">Register</Link> -->
+        </div>
     </div>
 </template>
