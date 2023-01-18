047453df591678ba0700ea06017fab89a1b640e6
Kakad3v <sule.appdev@gmail.com>
2023-01-18 22:19:17 +0300

    font color

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -13,8 +13,8 @@ defineProps({
     <Head title="Welcome" />
 
     <div
-        class="relative grid place-items-center min-h-screen bg-gray-900 dark:bg-gray-900 sm:items-center sm:pt-0"
+        class="relative px-3 grid place-items-center min-h-screen bg-gray-900 dark:bg-gray-900 sm:items-center sm:pt-0"
     >
-    <h1 class="text-8xl text-white">Coming soon...</h1>
+    <h1 class="text-3xl text-#E3F6FF">Coming soon...</h1>
     </div>
 </template>
