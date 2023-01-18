60300ddb40af81c169f15e287bac278245eef79d
Kakad3v <sule.appdev@gmail.com>
2023-01-18 22:39:09 +0300

    color

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -12,9 +12,7 @@ defineProps({
 <template>
     <Head title="Welcome" />
 
-    <div
-        class="relative px-3 grid place-items-center min-h-screen bg-gray-900 dark:bg-gray-900 sm:items-center sm:pt-0"
-    >
-    <h1 class="text-3xl text-[#E3F6FF]">Coming soon...</h1>
+    <div class="relative flex items-top justify-center min-h-screen bg-[#A3BB98] dark:bg-gray-900 sm:items-center sm:pt-0">
+<h1 class="text-4xl text-[#F0ECCF]">Coming Soon...</h1>
     </div>
 </template>
