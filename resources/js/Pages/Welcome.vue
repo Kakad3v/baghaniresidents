332168d50cbf81c529c90c5dc5f438111581edd8
Kakad3v <sule.appdev@gmail.com>
2023-01-18 22:03:27 +0300

    install breeze

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+});
+</script>
+
+<template>
+    <Head title="Welcome" />
+
+    <div
+        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0"
+    >
+    <h1>Coming soon...</h1>
+    </div>
+</template>
