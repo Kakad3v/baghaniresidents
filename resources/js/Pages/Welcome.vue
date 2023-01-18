0a190e27da9b034b5d40f74f4b0802c8dd29af3b
Kakad3v <sule.appdev@gmail.com>
2023-01-18 22:11:34 +0300

    Changed background color

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -13,8 +13,8 @@ defineProps({
     <Head title="Welcome" />
 
     <div
-        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0"
+        class="relative grid place-items-center min-h-screen bg-gray-900 dark:bg-gray-900 sm:items-center sm:pt-0"
     >
-    <h1>Coming soon...</h1>
+    <h1 class="text-3xl text-white">Coming soon...</h1>
     </div>
 </template>
