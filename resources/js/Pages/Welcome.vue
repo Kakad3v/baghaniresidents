852c1bfc70855643ecdb4dd89b7167adc2e57754
Kakad3v <sule.appdev@gmail.com>
2023-01-27 07:14:59 +0300

    Change link color

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,23 +2,21 @@
 import { Head, Link } from '@inertiajs/vue3';
 
 defineProps({
-    canLogin: Boolean,
-    canRegister: Boolean,
-    laravelVersion: String,
-    phpVersion: String,
+
 });
+
 </script>
 
 <template>
     <Head title="Welcome" />
 
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-        <div class="flex flex-col space-y-6 justify-center items-center">
+        <div class="flex flex-col space-y-6 justify-center items-center w-[93%] sm:w-1/4">
             <div>
-                <h1 class="text-xl font-bold text-gray-700">Baghani Residents Organization</h1>
+                <h1 class="text-2xl font-bold text-gray-700">Baghani Residents</h1>
             </div>
-            <Link :href="route('login')" class="w-full text-center text-lg rounded-lg px-8 py-2 shadow bg-white border">Proceed To Your Account</Link>
-            <Link :href="route('register')" class="w-full text-center text-lg rounded-lg px-8 py-2 shadow bg-black text-white">Become A Member</Link>
+            <Link :href="route('login')" class="w-full text-center text-xl  font-semibold tracking-wide rounded-lg px-8 py-2 shadow bg-white border">Proceed To Your Account</Link>
+            <Link :href="route('register')" class="w-full text-center text-xl  font-semibold tracking-wide rounded-lg px-8 py-2 shadow bg-black text-white">Become A Member</Link>
         </div>
     </div>
 </template>
